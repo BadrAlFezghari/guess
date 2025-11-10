c601d40d449952a921a7e8ced7ff4560a972b349
Badr AL FEZGHARI <157177720+BadrAlFezghari@users.noreply.github.com>
2025-11-10 21:11:07 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,19 @@
+nombre_fichiers=$(ls -1 | grep -v '^$' | wc -l)
 
+function demander_nombre(){
+read -p "Combien de fichiers se trouvent dans le répertoire actuel? " reponse
+}
+
+demander_nombre
+
+while true; do
+if [[ "$reponse" -eq "$nombre_fichiers" ]]; then
+echo "Félicitations, vous avez trouvé le bon nombre."
+break
+elif [[ "$reponse" -gt "$nombre_fichiers" ]]; then
+echo "Le nombre à deviner est trop basse."
+else
+echo "Le nombre à deviner est trop haute."
+fi
+demander_nombre
+done
